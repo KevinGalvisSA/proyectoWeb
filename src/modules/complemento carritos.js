dd906c23a088854eb8ab6fee45cdf873048daf2c
KevinGalvisSA <156678719+KevinGalvisSA@users.noreply.github.com>
2024-05-20 17:32:16 -0500

    feat: :alembic: Experimentando funcion de añadir productos

--- src/modules/complemento carritos.js
+++ src/modules/complemento carritos.js
@@ -3,28 +3,29 @@ import { LitElement, html, css } from "lit";
 class ToggleMenu extends LitElement {
   static styles = css`
     .button {
-        --width: 100px;
-        --height: 35px;
-        margin: 2em 1em 1em 2em;
-        --tooltip-height: 35px;
-        --tooltip-width: 100px;
-        --gap-between-tooltip-to-button: 18px;
-        --button-color: #222;
-        --tooltip-color: #fff;
-        width: var(--width);
-        height: var(--height);
-        background: #60EFB8;
-        position: relative;
-        text-align: center;
-        border-radius: 0.5em;
-        font-family: "Arial";
-        transition: background 0.7s;
+      --width: 100px;
+      --height: 35px;
+      margin: 2em 1em 1em 2em;
+      --tooltip-height: 35px;
+      --tooltip-width: 100px;
+      --gap-between-tooltip-to-button: 18px;
+      --button-color: #222;
+      --tooltip-color: #fff;
+      width: var(--width);
+      height: var(--height);
+      background: #60efb8;
+      position: relative;
+      text-align: center;
+      border-radius: 0.5em;
+      font-family: "Arial";
+      transition: background 0.7s;
     }
+
     .text {
       display: flex;
       align-items: center;
       justify-content: center;
-      background: #60EFB8
+      background: #60efb8;
     }
 
     .button-wrapper,
@@ -38,7 +39,6 @@ class ToggleMenu extends LitElement {
       color: #fff;
     }
 
-  
     .text:hover {
       cursor: pointer;
       background: var(--color-septimo);
@@ -55,22 +55,82 @@ class ToggleMenu extends LitElement {
 
   static properties = {
     open: { type: Boolean },
+    producto: { type: Object },
+    products: { type: Array },
+    nuevoProducto: { type: Object }
   };
 
   constructor() {
     super();
     this.open = false;
+    this.producto = {};
+    this.products = [];
   }
 
   toggleMenu() {
     this.open = !this.open;
   }
 
+  async idNameAbrigos() {
+    try {
+      let res = await fetch("http://localhost:5501/abrigo");
+      let data = await res.json();
+      let idsProductos = data.map(producto => producto.id);
+      return idsProductos;
+    } catch (error) {
+      console.error("Error al obtener los pedidos:", error);
+      return [];
+    }
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+    const coats = await this.idNameAbrigos();
+    this.products = coats;
+  }
+
+  async agregarAlCarrito() {
+    try {
+      // Obtener el carrito del endpoint correspondiente
+      let res = await fetch("http://localhost:5501/carrito");
+      let carrito = await res.json();
+
+      // Verificar si el producto ya está en el carrito
+      const productoEnCarrito = carrito.find(item => item[`${this.producto.categoria}id`] === this.producto.id);
+
+      // Si el producto está en el carrito, aumentar la cantidad
+      if (productoEnCarrito) {
+        productoEnCarrito.cantidad += 1;
+      } else {
+        // Si el producto no está en el carrito, agregarlo al carrito con cantidad 1
+        const nuevoProducto = {
+          [`${this.producto.categoria}id`]: this.producto.id,
+          cantidad: 1,
+          id: carrito.length + 1
+        };
+        carrito.push(nuevoProducto);
+      }
+
+      // Actualizar el carrito en el servidor
+      await fetch("http://localhost:5501/carrito",{
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(carrito)
+      });
+    } catch (error) {
+      console.error("Error al agregar el producto al carrito:", error);
+    }
+  }
+
   render() {
     return html`
       <div class="button" @click=${this.toggleMenu}>
         <div class="button-wrapper">
-          <button class="text">Agregar</button>
+          <button class="text" @click=${this.agregarAlCarrito}>
+            Agregar
+          </button>
         </div>
       </div>
     `;
@@ -78,12 +138,3 @@ class ToggleMenu extends LitElement {
 }
 
 customElements.define("toggle-menu", ToggleMenu);
-
-/*
-  Idea para adicion de productos:
-  - añadir un boton con un id igual al del producto
-  - filtrar el json para generar una funcion que revise el id del boton y compare con cada producto
-  - en caso de que se encuentre un id igual. al hacer click en el boton, añadir el producto al carrito haciendo push en el json.
-  - en caso de que no se encuentre un id igual. al hacer click en el boton, mostrar un alerta que diga que no se encuentra el producto.
-  - por ultimo revisar que se mantenga la reactividad en cada elemento
-*/
